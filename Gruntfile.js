e280a65e68091c7e5df523b27e02895180027ebf
Bartosz Piwek <bartoszpiwek@gmail.com>
2017-06-02 19:07:09 +0200

    working: database

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,7 +16,6 @@ module.exports = function(grunt) {
       all: [
         'Gruntfile.js',
         'tasks/*.js',
-        '<%= nodeunit.tests %>'
       ],
       options: {
         jshintrc: '.jshintrc'
@@ -41,11 +40,6 @@ module.exports = function(grunt) {
       }
     },
 
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js']
-    }
-
   });
 
   // Actually load this plugin's task(s).
@@ -58,7 +52,7 @@ module.exports = function(grunt) {
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'toolboxcss', 'nodeunit']);
+  grunt.registerTask('test', ['clean', 'toolboxcss']);
 
   // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'test']);
